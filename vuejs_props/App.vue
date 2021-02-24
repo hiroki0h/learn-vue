505a961ebf5ad756d3ce84c238f739ec538994e3
재연 <jjyeon@betree.co.kr>
2021-02-24 17:16:11 +0900

    10_10 싱글파일 컴포포넌트

---
+++ vuejs_props/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>  
+    <!-- <app-header v-bind:프롭스 속성 이름="상위 컴포넌트 데이터 이름"></app-header> -->
+    <app-header
+     v-bind:propsdata="headerName"
+     v-on:renew="renewHeaderName"
+    ></app-header>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue'
+
+export default {
+  data: function(){
+    return {
+      headerName: 'HEADER'
+    }
+  },
+  components: {
+    'app-header': AppHeader,
+    'app-footer': AppFooter
+  },
+  methods: {
+    renewHeaderName: function(){
+      this.headerName = "hi";
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ vuejs_props/components/AppFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer>
+    <h1>Footer</h1>
+  </footer>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+</style>
